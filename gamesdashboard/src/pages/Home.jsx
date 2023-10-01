49d87ca1b33e80b942e8fe91b3f4f6b49db9ff4c
Timothy <boydtim1@msu.edu>
2023-10-01 13:27:09 -0400

    Added CSS and styling for gamecard, like button, game rating, etc. Wrapped Gamecard in class called gameCard for styling ease

--- gamesdashboard/src/pages/Home.jsx
+++ gamesdashboard/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './home.css'
 import GameSwiper from '../components/GameSwiper'
+import GameCard from '../components/GameCard'
 
 function Home({games}) {
   return (
@@ -9,13 +10,20 @@ function Home({games}) {
             <div className="row">
                 <GameSwiper games={games}/>
             </div>
-            <div className="row">
+            <div className="row mb-4 mt-4">
                 <div className="col-lg-6">
                     <h2 className="sectionTitle">Games on Promotion</h2>
                 </div>
                 <div className="col-lg-6 d-flex justify-content-end align-items-center">
                    <a href="#" className="viewMore"> View More Games <i className="bi bi-arrow-right"></i></a>
                 </div>
+            </div>
+            <div className="row">
+                {
+                    games.slice(0,4).map(game=>(
+                        <GameCard key={game._id} game={game}/>
+                    ) //get items 0, 1, 2, 3 
+               ) }
             </div>
         </div>
     </section>
