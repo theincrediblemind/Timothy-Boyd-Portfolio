1bf0bb35b776be380c13526ab826839ab17b0551
Timothy <boydtim1@msu.edu>
2023-10-03 19:49:59 -0400

    Added Categories buttons to categories page that will allow for filtering by genre later

---
+++ gamesdashboard/src/data/filterListData.js
@@ -0,0 +1,42 @@
+const filterListData = [
+
+    {
+        _id: crypto.randomUUID(),
+        name: 'All',
+        active: 'true'
+    },
+
+    {
+        _id: crypto.randomUUID(),
+        name: 'RPG',
+        active: 'false'
+    },
+
+
+    {
+        _id: crypto.randomUUID(),
+        name: 'MOBA',
+        active: 'false'
+    },
+
+
+    {
+        _id: crypto.randomUUID(),
+        name: 'Battle',
+        active: 'false'
+    },
+
+    {
+        _id: crypto.randomUUID(),
+        name: 'Racing',
+        active: 'false'
+    },
+
+    {
+        _id: crypto.randomUUID(),
+        name: 'Fighting',
+        active: 'false'
+    }
+]
+
+export default filterListData;
--- gamesdashboard/src/pages/Categories.jsx
+++ gamesdashboard/src/pages/Categories.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './categories.css'
+import filterListData from '../data/filterListData.js'
 
-function Categories({games, reference}) {
+function Categories({games, reference, sectionActive}) {
+    const [filters, setFilters] = useState(filterListData);
+
+
   return (
     <section id="categories" className='categories' ref={reference}>
-        <h1>Category</h1>
+        <div className="container-fluid mt-2">
+            <div className="row">
+                <div className="col-lg-8">
+                    <ul className="filters">
+                        {
+                            filters.map(filter=>(<li key={filter._id}>{filter.name}</li>))
+                        }
+                    </ul>
+                </div>
+                <div className="col-lg-4"></div>
+            </div>
+        </div>
     </section>
   )
 }
